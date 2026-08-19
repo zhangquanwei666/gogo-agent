ce002325bfcb0c9b8233b68fc2aa8f4f2fb6c2fb
zhangquanwei <zhangquanwei@sunrate.com>
2026-08-19 10:50:26 +0800

    first commit

---
+++ src/main/java/com/quanwei/gogo/agent/GogoAgentApplication.java
@@ -0,0 +1,13 @@
+package com.quanwei.gogo.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogoAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GogoAgentApplication.class, args);
+    }
+
+}
